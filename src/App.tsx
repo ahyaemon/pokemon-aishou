81ce871a661b845d77008b9796d8e7750a35867b
ahyaemon <iamahyaemon@gmail.com>
2021-06-05 23:14:49 +0900

    test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router hashType="noslash">
+          <div className="App">
+              <h1>pokemon aishou</h1>
+              <hr/>
+              <Link to="/">top</Link>
+              <br/>
+              <Link to="/about/a">about</Link>
+          </div>
+          <Switch>
+              <Route path="/about/a">about</Route>
+              <Route path="/">top</Route>
+          </Switch>
+      </Router>
   );
 }
 
